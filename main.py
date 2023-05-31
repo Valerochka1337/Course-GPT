50415548f1c611deeb4c6b8ab45f23eaf2f55cee
Valerochka1337 <valerochkayartsev88@gmail.com>
2023-05-31 16:22:53 +0300

    echobot created

---
+++ main.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, types
+from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import Dispatcher
+
+BOT_TOKEN = '6254843237:AAGQ2PzWCtaYdQsjgRw6s-iyzUTm2d_euhQ'
+
+storage = MemoryStorage()
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher(bot, storage=storage)
+
+
+@dp.message_handler()
+async def respond(message: types.Message):
+    await message.reply(message.text)
+
+
+# Start the bots polling loop
+if __name__ == '__main__':
+    executor.start_polling(dp)
