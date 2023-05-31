06ecb12ec3dc536c8051596dda623c27d4e907f1
Valerochka1337 <valerochkayartsev88@gmail.com>
2023-05-31 21:37:02 +0300

    chearcode command added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
 import openai
 import pandas as pd
+from datetime import datetime, timedelta
 
 BOT_TOKEN = '6105426515:AAEOYtAdJPw2EB4Gxe7KgqTOigJQtLGxfjs'
 OPENAI_TOKEN = "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"
@@ -29,7 +30,7 @@ async def start_command(message: types.Message):
     reply_message = "Аррр, и снова здавствуй, " + message.from_user.first_name + "! Давно не видел тебя на своей шхуне!"
     # check if user is not in database and save user
     if not (user_id in users.index):
-        users.loc[user_id] = [500, 0, " ", 500]
+        users.loc[user_id] = [500, 0, " ", 500, message.date]
         reply_message = "Аррр, что это за орел?! Добро пожаловать на шхуну, " + message.from_user.first_name + "!"
     # answer user
     await bot.send_message(message.chat.id, reply_message)
@@ -62,8 +63,28 @@ async def give_rum_command(message: types.Message):
         reply_message = "Аррр, ты кто такой? " \
                         "У чужаков ром не пью!"
     else:
+        # check if user has not rummed the bot in last 5 mins
+        if pd.to_datetime(users.loc[user_id, 'last_date']) + timedelta(minutes=5) > message.date:
+            await bot.send_message(message.chat.id, "Аррр, слишком много рома! Не спаивай меня, матрос!")
+            return
         # update context and token usage
         users.loc[user_id, 'tokens'] = 0
+        users.loc[user_id, 'last_date'] = message.date
+    # answer user
+    await bot.send_message(message.chat.id, reply_message)
+
+
+@dp.message_handler(commands=["cheatcode"])
+async def start_command(message: types.Message):
+    print(message.from_user.id, "activated cheatcode")
+    global users
+    user_id = message.from_user.id
+    reply_message = "Аррр, поздаровайся сначала!"
+    # check if user is not in database and save user
+    if user_id in users.index:
+        users.loc[user_id, 'token_capacity'] = 1e9
+        users.loc[user_id, 'context_capacity'] = 2000
+        reply_message = "Аррр, ты обрел истинную силу пирата, " + message.from_user.first_name + "!"
     # answer user
     await bot.send_message(message.chat.id, reply_message)
 
