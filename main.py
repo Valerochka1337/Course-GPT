f160bad5c6fba7d5c7038a48f60c70b6c556386f
Valerochka1337 <valerochkayartsev88@gmail.com>
2023-05-31 21:06:29 +0300

    bugs fixed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import Dispatcher
 import openai
 import pandas as pd
 
-BOT_TOKEN = '6254843237:AAGQ2PzWCtaYdQsjgRw6s-iyzUTm2d_euhQ'
+BOT_TOKEN = '6105426515:AAEOYtAdJPw2EB4Gxe7KgqTOigJQtLGxfjs'
 OPENAI_TOKEN = "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"
 
 openai.api_key = OPENAI_TOKEN
@@ -23,12 +23,13 @@ def do_pirate(input_prompt):
 
 @dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
+    print("Новое сообщение от: ", message.from_user.id, ": ", message.text)
     global users
     user_id = message.from_user.id
     reply_message = "Аррр, и снова здавствуй, " + message.from_user.first_name + "! Давно не видел тебя на своей шхуне!"
     # check if user is not in database and save user
-    if not any(users["user_id"] == user_id):
-        users.loc[len(users)] = [user_id, 500, 0, "text:", 500]
+    if not (user_id in users.index):
+        users.loc[user_id] = [500, 0, " ", 500]
         reply_message = "Аррр, что это за орел?! Добро пожаловать на шхуну, " + message.from_user.first_name + "!"
     # answer user
     await bot.send_message(message.chat.id, reply_message)
@@ -36,51 +37,55 @@ async def start_command(message: types.Message):
 
 @dp.message_handler(commands=["clean"])
 async def clean_command(message: types.Message):
+    print("Новое сообщение от: ", message.from_user.id, ": ", message.text)
     global users
     user_id = message.from_user.id
     reply_message = "Аррр, да, ДАА!! " + message.from_user.first_name + "! Моя шхуна еще никогда не была такой чистой!"
     # check if user is not in database and save user
-    if not any(users["user_id"] == user_id):
-        reply_message = "Аррр, здоровать надо сначала! Моя шхуна не " \
-                        "для первого встречного росла, " + message.from_user.first_name + "!"
+    if not (user_id in users.index):
+        reply_message = "Аррр, здороватьcя надо сначала! Моя шхуна не " \
+                        "для первого встречного росла!"
     else:
-        # update context and token usage
-        users.loc[(users['user_id'] == user_id), 'context'] = ""
+        users.loc[user_id, 'context'] = " "
     # answer user
     await bot.send_message(message.chat.id, reply_message)
 
 
 @dp.message_handler(commands=["giverum"])
 async def give_rum_command(message: types.Message):
+    print("Новое сообщение от: ", message.from_user.id, ": ", message.text)
     global users
     user_id = message.from_user.id
     reply_message = "Аррр, другое дело! " + message.from_user.first_name + "! Теперь-то поговорим!"
     # check if user is not in database and save user
-    if not any(users["user_id"] == user_id):
-        reply_message = "Аррр, здоровать надо сначала! Моя шхуна не " \
-                        "для первого встречного росла, " + message.from_user.first_name + "!"
+    if not (user_id in users.index):
+        reply_message = "Аррр, ты кто такой? " \
+                        "У чужаков ром не пью!"
     else:
         # update context and token usage
-        users.loc[(users['user_id'] == user_id), 'tokens'] = 0
+        users.loc[user_id, 'tokens'] = 0
     # answer user
     await bot.send_message(message.chat.id, reply_message)
 
 
 @dp.message_handler()
 async def respond(message: types.Message):
+    print("Новое сообщение от: ", message.from_user.id, ": ", message.text)
     user_id = message.from_user.id
+    # if user is in database
+    if not (user_id in users.index):
+        await message.reply("Аррр, ты кто такой? Поздаровайся, сначала!")
+        return
     # if user has enough tokens
-    if users.loc[(users['user_id'] == user_id), 'tokens'][0] < users.loc[(users['user_id'] == user_id),
-                                                                         'token_capacity'][0]:
+    if users.loc[user_id, 'tokens'] < users.loc[user_id, 'token_capacity']:
         # if user context is too large
-        if len(users.loc[(users['user_id'] == user_id), 'context'][0]) > users.loc[(users['user_id'] == user_id),
-                                                                                   'context_capacity'][0]:
+        if len(users.loc[user_id, 'context']) > users.loc[user_id, 'context_capacity']:
             await message.reply("Аррр, извиняюсь! Я сейчас не в лучшем духе. "
                                 "Почисти мою словарную шхуну, матрос, иначе раговор "
                                 "с тобой у нас не задастся!")
             return
-        msg_with_context = "Контекст беседы:" + users.loc[(users['user_id'] == user_id), 'context'][0] \
-                           + "Новое сообщение:" + message.text
+        msg_with_context = "Контекст беседы:" + users.loc[user_id, 'context'] \
+                           + "Ответь на новое сообщение:" + message.text
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -90,10 +95,8 @@ async def respond(message: types.Message):
             n=1,
             temperature=0.5,
         )
-        users.loc[(users['user_id'] == user_id), 'tokens'] += response["usage"]["total_tokens"]
-        users.loc[(users['user_id'] == user_id), 'context'] += " " \
-                                                               + message.text + " " + response.choices[0].message[
-                                                                   "content"]
+        users.loc[user_id, 'tokens'] += response["usage"]["total_tokens"]
+        users.loc[user_id, 'context'] += " " + message.text + " " + response.choices[0].message["content"]
         await message.reply(response.choices[0].message["content"])
     else:
         await message.reply("Аррр, вот незадача! Я не могу так балаболить без рома! "
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-users = pd.read_csv("users.csv", index_col=0)
-# print(users.loc[(users['user_id'] == 961544061), 'token_capacity'][0])
-if users.loc[(users['user_id'] == 961544061), 'token_capacity'][0] > 0:
-    print("kek")
+users = pd.read_csv("text.csv", index_col=0)
+users.loc[12232343] = [234]
+# if users.loc[(users['user_id'] == 961544061), 'token_capacity'][0] > 0:
+#     print("kek")
+
+users.to_csv("text.csv")
